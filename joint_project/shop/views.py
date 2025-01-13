7e57048984bb97e71bbd8ba07aa828abc273bb19
Vladislav <takejji@gmail.com>
2025-01-13 19:11:06 +0300

    fix: Основные функции изменены на Классы представленийй feat: Добавлен класс регистрации и входа/выхода

--- joint_project/shop/views.py
+++ joint_project/shop/views.py
@@ -1,19 +1,82 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Shop, Category
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+from django.views.generic import ListView, DetailView
+
+from .models import Product, Category
 
 # Create your views here.
 
-def index(requests):
-    shop = Shop.objects.all()
-    context = {'title': 'Главная', 'shops': shop}
-    return render(requests, 'shop/index.html', context)
 
-def detain_goods(requests, pk):
-    shop = get_object_or_404(Shop, pk=pk)
-    context = {'title': shop.name, 'shop': shop}
-    return render(requests, 'shop/detail.html', context)
+class IndexView(ListView):
+    model = Product
+    template_name = 'shop/index.html'
+    context_object_name = 'products'
 
-def category(requests):
-    cats = Category.objects.all()
-    context = {'title': 'Категории', 'cats': cats}
-    return render(requests, 'shop/category.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная'
+        return context
+
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'shop/detail.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.name
+        return context
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'shop/category.html'
+    context_object_name = 'categories'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категории'
+        return context
+
+
+class RegisterView(View):
+    template_name = 'shop/register.html'
+
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        return render(request, self.template_name, {'form': form})
+
+
+class LoginView(View):
+    template_name = 'shop/login.html'
+
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('index')
+        return render(request, self.template_name, {'form': form})
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('index')
+
