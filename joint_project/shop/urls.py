ffeb297e5c956433af2ee152c1e8905eedc0c4f4
Vladislav <takejji@gmail.com>
2025-01-13 19:47:24 +0300

    fix: изменил маршруты и добавил новую функцию категорий

--- joint_project/shop/urls.py
+++ joint_project/shop/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('categories/', CategoryListView.as_view(), name='categories'),
+    path('categories/<int:category_id>/', ProductsByCategoryView.as_view(), name='products_by_category'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
