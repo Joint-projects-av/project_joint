46f8e0f5bc93780da0e28cc8fa2a68786e9011b3
Vladislav <takejji@gmail.com>
2025-01-13 18:53:44 +0300

    feat: Добавлены модели базы данных + Product + Cart + CartItem + Order

--- joint_project/shop/models.py
+++ joint_project/shop/models.py
@@ -1,16 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
 
 class Category(models.Model):
-    title = models.CharField(max_length=255)
-    is_actual = models.BooleanField(default=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
 
-class Shop(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
-    color = models.CharField(max_length=255)
-    count = models.IntegerField(null=True)
-    price = models.IntegerField(null=True)
-    is_actual = models.BooleanField(default=True)
-    categories = models.ForeignKey('Category', null=True, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='CartItem')
+
+    def __str__(self):
+        return f"Корзина ({self.user.username})"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Заказ ({self.id},  {self.user.username})"
